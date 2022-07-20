d03e925cea5a76e4dbb2ef20291798893c7d4710
yann.saintmarc <yann.saintmarc@gmail.com>
2022-07-20 23:39:58 +0200

    second commit - test 2

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 require('dotenv').config();
 const express = require ('express');
 const cors = require('cors');
-
+//const session =require ('express-session');
 const router = require('./router');
 
-
+const app = express();
 
 app.use(express.json());
 app.use(cors('*'));
 app.use(router);
-app.use(express.static(--dirname + '/public'));
-
+app.use(express.static('/public'));
+//app.use(session);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server running on :', process.env.PORT);
---
+++ app/models/index.js
@@ -0,0 +1,18 @@
+//const example = require('./example');
+const users = require('./users');
+const media = require('./media');
+const theme = require('./theme');
+const ressources = require('./ressources');
+const workspace = require('./workspace');
+
+
+// code ici
+// example.hasMany(anotherExample,{});
+
+module.exports = {
+    users,
+    media,
+    theme,
+    ressources,
+    workspace
+}
